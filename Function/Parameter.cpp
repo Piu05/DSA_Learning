06e29e7a94ebd97341fecd9ccabcf14603173016
Piu05 <priyanka050412@gmail.com>
2022-01-10 23:20:40 +0530

    Parameter Passing Methods

--- Function/Parameter.cpp
+++ Function/Parameter.cpp
@@ -3,6 +3,24 @@
 //1.Call by Value
 #include<iostream>
 using namespace std;
+void swap(int x,int y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+int main()
+{
+    int a,b;
+    a=10;
+    b=5;
+    cout<<a<<endl<<b<<endl;
+    swap(a,b);
+    cout<<a<<endl<<b; //here value will not change i.e.,a=10 b=5 bcoz actual parameters will not change bcoz of formal parameters
+    return 0;
+}
+
 //Call by address 
 void swap(int *x,int *y)
 {
@@ -18,6 +36,25 @@ int main()
     b=5;
     cout<<a<<endl<<b<<endl;
     swap(&a,&b);
-    cout<<a<<endl<<b;
+    cout<<a<<endl<<b;       //Swapping will be done and values will be interchanged
+    return 0;
+}
+
+//Call by Reference (used in c++, cannot be used often)
+void swap(int &x,int &y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+int main()
+{
+    int a,b;
+    a=10;
+    b=5;
+    cout<<a<<endl<<b<<endl;
+    swap(a,b);
+    cout<<a<<endl<<b;       //Swapping will be done and values will be interchanged
     return 0;
 }
