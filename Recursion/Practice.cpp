35b2cbec0b5f4a5e086908913b8b49aee4fb3ba0
Piu05 <priyanka050412@gmail.com>
2022-01-17 22:12:30 +0530

    Recursion practice program

--- Recursion/Practice.cpp
+++ Recursion/Practice.cpp
@@ -1,5 +1,17 @@
+/* Recursion practice topics
+Menu driven program for:
+1.Sum of n natural number
+2.Factorial of a number
+*/
 #include<iostream>
 using namespace std;
+int sum(int n)
+{
+    if(n==0)
+        return 0;
+    else
+        return sum(n-1)+n;
+}
 int factorial(int n)
 {
     if(n==0)
@@ -8,8 +20,22 @@ int factorial(int n)
 }
 int main()
 {
-    int x;
-    cout<<"Enter a number: ";
-    cin>>x;
-    cout<<"/nFactorial of "<<x<<" is :"<<factorial(x);
+    int x,choice;
+    cout<<"*****MENU*****\n1.Sum of n numbers\n2.Factorial of a number";
+    cout<<"\nEnter choice: ";
+    cin>>choice;
+    switch(choice)
+    {
+        case 1: cout<<"Enter a number: ";
+                cin>>x;
+                cout<<"Sum of "<<x<<" numbers :"<<sum(x);
+                break;
+        case 2: cout<<"Enter a number: ";
+                cin>>x;
+                cout<<"Factorial of "<<x<<" is :"<<factorial(x);
+                break;
+        default:cout<<"Wrong choice!!";
+                break;
+    }
+    return 0;
 }
