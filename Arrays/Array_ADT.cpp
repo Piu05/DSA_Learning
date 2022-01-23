a6a863670169a661b18a7a99f226ad8f8bb02aa7
Piu05 <priyanka050412@gmail.com>
2022-01-23 22:24:51 +0530

    Array ADT Operations

--- Arrays/Array_ADT.cpp
+++ Arrays/Array_ADT.cpp
@@ -29,8 +29,11 @@ struct Array
 //For append
 void append(Array *arr,int x)
 {
-    if(arr->length<arr->size)
-        arr->A[arr->length++]=x;
+    if((arr->length)<(arr->size))
+    {
+        arr->A[arr->length]=x;
+        arr->length++;
+    }
 }
 //For display 
 void display(Array arr)
@@ -39,10 +42,32 @@ void display(Array arr)
     for(int i=0;i<arr.length;i++)
         cout<<arr.A[i]<<" ";
 }
+//For insert
+void insert(Array *arr,int index,int x)
+{
+    if(index>=0 && index<=(arr->length))
+    {
+        for(int i=arr->length;i>index;i--)
+        {
+            arr->A[i]=arr->A[i-1];
+        }
+        arr->A[index]=x;
+        arr->length++;
+    }
+}
+//For delete
+void Delete(Array *arr,int index)
+{
+    int x;
+    x=arr->A[index];
+    for(int i=index;i<arr->length;i++)
+        arr->A[i]=arr->A[i+1];
+    arr->length--;
+}
 int main()
 {
     Array arr;
-    int n,value;
+    int n,index,value1,value2;
     cout<<"Enter no. of elements:";
     cin>>n;
     cout<<"Enter elements: ";
@@ -53,9 +78,19 @@ int main()
     arr.length=n;
     display(arr);
     cout<<"\nEnter value to append: ";
-    cin>>value;
-    append(&arr,value);
+    cin>>value1;
+    append(&arr,value1);
     cout<<"After appending ";
+    display(arr);
+    cout<<"\nEnter index and value to insert: ";
+    cin>>index>>value2;
+    insert(&arr,index,value2);
+    cout<<"After inserting ";
+    display(arr);
+    cout<<"\nEnter index to delete: ";
+    cin>>index;
+    Delete(&arr,index);
+    cout<<"After deleting ";
     display(arr);
     return 0;
 }
