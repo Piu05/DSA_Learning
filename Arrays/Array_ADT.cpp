14f02a52a18e501851fe461ce6965a196de46308
Piu05 <priyanka050412@gmail.com>
2022-01-24 23:41:01 +0530

    ADT operations

--- Arrays/Array_ADT.cpp
+++ Arrays/Array_ADT.cpp
@@ -64,10 +64,61 @@ void Delete(Array *arr,int index)
         arr->A[i]=arr->A[i+1];
     arr->length--;
 }
+//For getting a value
+int Get(Array arr,int index)
+{
+    if(index>=0 && index<arr.length)
+        return arr.A[index];
+    return -1;
+}
+//For setting a value
+void Set(Array *arr,int index,int x)
+{
+    if(index>=0 && index<(arr->length))
+    arr->A[index]=x;
+}
+//For finding maximum value
+int Max(Array arr)
+{
+    int max=arr.A[0];
+    for(int i=1;i<arr.length;i++)
+    {
+        if(arr.A[i]>max)
+            max=arr.A[i];
+    }
+    return max;
+}
+//For finding minimum value
+int Min(Array arr)
+{
+    int min=arr.A[0];
+    for(int i=1;i<arr.length;i++)
+    {
+        if(arr.A[i]<min)
+            min=arr.A[i];
+    }
+    return min;
+}
+//For sum 
+int Sum(Array arr)
+{
+    int sum=0;
+    for(int i=0;i<arr.length;i++)
+        sum+=arr.A[i];
+    return sum;
+}
+//For average
+float Avg(Array arr)
+{
+    int sum=0;
+    for(int i=0;i<arr.length;i++)
+        sum+=arr.A[i];
+    return sum/arr.length;
+}
 int main()
 {
     Array arr;
-    int n,index,value1,value2;
+    int n,index,value1,value2,a;
     cout<<"Enter no. of elements:";
     cin>>n;
     cout<<"Enter elements: ";
@@ -76,21 +127,48 @@ int main()
         cin>>arr.A[i];
     }
     arr.length=n;
-    display(arr);
-    cout<<"\nEnter value to append: ";
-    cin>>value1;
-    append(&arr,value1);
-    cout<<"After appending ";
-    display(arr);
-    cout<<"\nEnter index and value to insert: ";
-    cin>>index>>value2;
-    insert(&arr,index,value2);
-    cout<<"After inserting ";
-    display(arr);
-    cout<<"\nEnter index to delete: ";
-    cin>>index;
-    Delete(&arr,index);
-    cout<<"After deleting ";
-    display(arr);
+    cout<<"*****MENU*****";
+    cout<<"1.Append\n2.Insert\n3.Delete\n4.Display\n5.Get\n6.Set\n7.Max\n8.Min\n9.Sum\n10.Average\n11.Exit";
+    cin>>a;
+    switch(a)
+    {
+        case 1: cout<<"\nEnter value to append: ";
+                cin>>value1;
+                append(&arr,value1);
+                cout<<"After appending ";
+                display(arr);
+                break;
+        case 2: cout<<"\nEnter index and value to insert: ";
+                cin>>index>>value2;
+                insert(&arr,index,value2);
+                cout<<"After inserting ";
+                display(arr);
+                break;
+        case 3: cout<<"\nEnter index to delete: ";
+                cin>>index;
+                Delete(&arr,index);
+                cout<<"After deleting ";
+                display(arr);
+                break;
+        case 4: display(arr);
+                break;
+        case 5: cout<<"Enter the index: ";
+                cin>>index;
+                Get(arr,index);
+                break;
+        case 6: cout<<"Enter the index: ";
+                cin>>index;
+                cout<<"Enter the value: ";
+                cin>>value1;
+                Set(&arr,index,value1);
+                display(arr);
+                break;
+        case 7: cout<<"Maximum value is: "<<Max(arr);
+                break;
+        case 8: cout<<"Minimum value is: "<<Min(arr);
+                break;
+        default:"Wrong option";
+                break;
+    }
     return 0;
 }
