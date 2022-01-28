446b69132e1d8a55aa3e89656e357b09d806410d
Piu05 <priyanka050412@gmail.com>
2022-01-28 23:48:33 +0530

    Array Based Problems

--- Arrays/Array_Practice.cpp
+++ Arrays/Array_Practice.cpp
@@ -92,9 +92,46 @@ void FindSum(int A[],int sum,int n)
 {
     for(int i=0;i<n;i++)
     {
-        
+        for(int j=i+1;j<n;j++)
+        {
+            if(A[i]+A[j]==sum)
+                cout<<"\nPair with sum "<<sum<<" is "<<A[i]<<","<<A[j];
+        }
     }
-
+}
+//Finding pair of elements with sum k using hash table
+void FindSum2(int A[],int h,int n,int sum)
+{
+    int A2[h];
+    for(int i=0;i<h;i++)
+    {
+        A2[i]=0;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(A2[sum-A[i]]!=0 && sum-A[i]>0)
+            cout<<"\nPair with sum "<<sum<<" is:"<<A[i]<<" "<<sum-A[i];
+        A2[A[i]]++;
+    }
+}
+//Find pair of elements with sum k in sorted array
+void FindSumS(int A[],int sum, int n)
+{
+    int i=0;
+    int j=n-1;
+    while(i<j)
+    {
+        if(A[i]+A[j]==sum)
+        {
+            cout<<"\nPair is "<<A[i]<<","<<A[j];
+            i++;
+            j--;
+        }
+        else if(A[i]+A[j]<sum)
+            i++;
+        else
+            j--;
+    }
 }
 int main()
 {
@@ -103,6 +140,8 @@ int main()
     int C[10]={3,7,4,9,12,6,1,11,2,10};    
     int D[10]={3,4,8,8,12,13,15,15,15,18};
     int E[10]={8,12,13,15,3,4,8,15,15,18};
+    int F[10]={6,3,8,10,16,7,5,2,9,14};
+    int G[10]={1,3,4,5,6,7,8,9,10,12};
     //for single element missing 
     int a=MissingElement(A,11);
     if(a==-1)
@@ -111,11 +150,17 @@ int main()
         cout<<"The missing no. is => "<<a;
     //Method 1 for multiple elements missing
     MissingElements(B,6,11);
-    //Method 2
+    //Method 2 using hash table
     MissingElements2(C,1,12,10);
     //Duplicate
     Duplicate(D,10);
-    //Duplicate 2
+    //Duplicate 2 using hash table 
     Duplicate2(E,18,10);
+    //Find pair of elements with sum k
+    FindSum(F,10,10);
+    //Find pair of elements with sum k using hash table
+    FindSum2(F,16,10,10);
+    //Find pair of elements with sum k in sorted array
+    FindSumS(G,10,10);
     return 0;
 }
