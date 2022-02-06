da55811ebd1c8bf42af2770549aadf04e35f6cf9
Piu05 <priyanka050412@gmail.com>
2022-02-06 23:42:45 +0530

    Linked List create and display

--- Linked_List/LinkedList.cpp
+++ Linked_List/LinkedList.cpp
@@ -10,12 +10,47 @@ struct Node         //Self-referential structure (structure pointing to itself)
 {
     int data;
     Node *next;
-};
+}*first=NULL;
+void create(int A[],int n)
+{
+    Node *t,*last;
+    first=new Node;
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+    for(int i=1;i<n;i++)
+    {
+        t=new Node;
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+//Display using loop
+void display(Node *p)
+{
+    while(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+//Display using recursive function      //time complexity-O(n) [stack size=n+1]
+void Display(Node *p)
+{
+    if(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        Display(p->next);
+    }
+}
 int main()
 {
-    Node *p;
-    p=new Node;
-    p->data=10;
-    p->next=NULL;
+    int A[]={3,5,7,10,15};
+    create(A,5);
+    display(first);
+    cout<<endl;
+    Display(first);
     return 0;
 }
