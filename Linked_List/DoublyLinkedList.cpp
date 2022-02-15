124dc1b46b65dd255d967987d150a675e53d482e
Piu05 <priyanka050412@gmail.com>
2022-02-15 22:10:24 +0530

    Doubly linked list program

--- Linked_List/DoublyLinkedList.cpp
+++ Linked_List/DoublyLinkedList.cpp
@@ -11,25 +11,30 @@ struct Node
     int data;
     Node *next;
 }*first=NULL;
+Node* CreateNode(int x)
+{
+    Node *t=new Node;
+    t->data=x;
+    t->prev=NULL;
+    t->next=NULL;
+    return t;
+}
 void create(int A[],int n)
 {
     Node *t,*last;
     int i;
-    first=new Node;
-    first->data=A[0];
-    first->prev=NULL;
-    first->next=NULL;
+    first=CreateNode(A[0]);
     last=first;
     for(i=1;i<n;i++)
     {
-        t=new Node;
-        t->data=A[i];
+        t=CreateNode(A[i]);
         t->prev=last;
         t->next=NULL;
+        last->next=t;
         last=t;
     }
 }
-void display(Node *p)           //Error
+void display(Node *p)         
 {
     while(p!=NULL)
     {
@@ -48,33 +53,45 @@ int length(Node *p)
     }
     return len;
 }
-///////////
-void Insert(Node *p,int n,int x)
+void Insert(Node *p,int pos,int x)
 {
-    Node *t,*last;
+    Node *t;
     int i;
-    first=new Node;
-    first->data=n;
-    first->prev=NULL;
-    first->next=NULL;
-    last=first;
-    for(i=1;i<n;i++)
+    if(pos<0 || pos>length(p))
+        return;
+    if(pos==0)
     {
         t=new Node;
         t->data=x;
-        t->prev=last;
-        t->next=NULL;
-        last=t;
+        t->prev=NULL;
+        t->next=first;
+        first->prev=t;
+        first=t;
+    }
+    else
+    {
+        for(i=0;i<pos-1;i++)
+            p=p->next;
+        t=new Node;
+        t->data=x;
+        t->prev=p;
+        t->next=p->next;
+        if(p->next)p->next->prev=t;
+        p->next=t;
     }
 }
-void Reverse(Node *p)           //Error
+void Reverse(Node *p)
 {
+    Node *temp;
     while(p!=NULL)
     {
-        cout<<p->data<<" ";
-        p=p->next;
+        temp=p->next;
+        p->next=p->prev;
+        p->prev=temp;
+        p=p->prev;
+        if(p!=NULL && p->next==NULL)
+            first=p;
     }
-    cout<<endl;
 }
 int Delete(Node *p,int pos)
 {
@@ -106,11 +123,19 @@ int main()
     int A[]={1,3,5,7,9};
     create(A,5);
     display(first);
-    cout<<"\nLength: "<<length(first);
+    cout<<"\nLength: "<<length(first)<<endl;
     Insert(first,2,25);
+    display(first);
     Reverse(first);
     display(first);
-    Delete(first,1);
+    Delete(first,4);
     display(first);
     return 0;
 }
+//Other linked list functions will be same as singly linked list only the node positions will be different
+//Circular Doubly linked list
+/*
+If we want to add new node before head node then we can just go to last node bu using head->prev
+It can be done in constant time without traversing to whole list
+Should write program for insertion and deletion in circular doubly linked list as practice
+*/
