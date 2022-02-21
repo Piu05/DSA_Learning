3946bf07c9d567400ba2ebaadbd8659b223c786f
Piu05 <priyanka050412@gmail.com>
2022-02-21 23:22:49 +0530

    Queue ADT using C and C++

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -74,3 +74,61 @@ int main()
     display(q);
     return 0;
 }
+
+//In C++, queue can be implemented using class:
+/*class Queue
+{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+    public:
+    Queue(){front=rear=-1;  size=10;    Q=new int[size];}
+    Queue(int size){front=rear=-1;  this->size=size;    Q=new int[this->size]; }
+    void enqueue(int x);
+    int dequeue();
+    void display();
+};
+void Queue::enqueue(int x)
+{
+    if(rear==size-1)
+        cout<<"\nQueue is full";
+    else
+    {
+        rear++;
+        Q[rear]=x;
+    }
+}
+int Queue::dequeue()
+{
+    int x=-1;
+    if(front==rear)
+        cout<<"\nQueue is empty";
+    else
+    {
+        front++;
+        x=Q[front];
+    }
+    return x;
+}
+void Queue::display()
+{
+    for(int i=front+1;i<=rear;i++)
+    {
+        cout<<Q[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    Queue q(5);
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.display();
+    q.dequeue();
+    q.display();
+    return 0;
+}
+*/
